5de78785f6cc846f822391cdbeb7ca3b21057c85
asxz0 <asxz0121@gmail.com>
2022-07-15 21:11:49 +0800

    notification manager

---
+++ app/src/main/java/com/jordan/audioplayer/media/exoplayer/MediaPlayerNotificationManager.kt
@@ -0,0 +1,76 @@
+package com.jordan.audioplayer.media.exoplayer
+
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.jordan.audioplayer.R
+import com.jordan.audioplayer.media.constants.K
+
+internal class MediaPlayerNotificationManager(
+    context: Context,
+    sessionToken: MediaSessionCompat.Token,
+    notificationListener: PlayerNotificationManager.NotificationListener
+) {
+    private val notificationManager: PlayerNotificationManager
+
+    init {
+        val mediaController = MediaControllerCompat(context, sessionToken)
+
+        val builder = PlayerNotificationManager.Builder(
+            context,
+            K.PLAYBACK_NOTIFICATION_ID,
+            K.PLAYBACK_NOTIFICATION_CHANNEL_ID
+        )
+
+        with(builder) {
+            setMediaDescriptionAdapter(DescriptionAdapter(mediaController))
+            setNotificationListener(notificationListener)
+            setChannelNameResourceId(R.string.notification_channel)
+            setChannelDescriptionResourceId(R.string.notification_channel_description)
+        }
+
+        notificationManager = builder.build()
+
+        with(notificationManager) {
+            setMediaSessionToken(sessionToken)
+            setSmallIcon(R.drawable.ic_music_note)
+            setUseRewindAction(false)
+            setUseFastForwardAction(false)
+        }
+    }
+
+    fun hideNotificationManager() {
+        notificationManager.setPlayer(null)
+    }
+
+    fun showNotificationManager(player: Player) {
+        notificationManager.setPlayer(player)
+    }
+
+    inner class DescriptionAdapter(
+        private val controller: MediaControllerCompat
+    ): PlayerNotificationManager.MediaDescriptionAdapter {
+        override fun getCurrentContentTitle(player: Player): CharSequence {
+            return controller.metadata.description.title.toString()
+        }
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? {
+            return controller.sessionActivity
+        }
+
+        override fun getCurrentContentText(player: Player): CharSequence? {
+            return controller.metadata.description.subtitle
+        }
+
+        override fun getCurrentLargeIcon(
+            player: Player,
+            callback: PlayerNotificationManager.BitmapCallback
+        ): Bitmap? {
+            return null
+        }
+    }
+}
--- app/src/main/java/com/jordan/audioplayer/media/service/MediaPlayerService.kt
+++ app/src/main/java/com/jordan/audioplayer/media/service/MediaPlayerService.kt
@@ -2,11 +2,17 @@ package com.jordan.audioplayer.media.service
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource
 import com.jordan.audioplayer.media.constants.K
+import com.jordan.audioplayer.media.exoplayer.MediaSource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -17,6 +23,16 @@ class MediaPlayerService: MediaBrowserServiceCompat() {
 
     @Inject
     lateinit var exoPlayer: ExoPlayer
+
+    @Inject
+    lateinit var mediaSource: MediaSource
+
+    private val serviceJob = SupervisorJob()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    // mediaSession can use to control playback
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
 
     override fun onGetRoot(
         clientPackageName: String,
